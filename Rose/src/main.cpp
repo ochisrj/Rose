41b005530f8d71a0dc2b71b53489de7248a0bfee
ochisrj <kittiphumisarayci57@gmail.com>
2026-09-18 11:45:10 +0700

    refresh engine

--- Rose/src/main.cpp
+++ Rose/src/main.cpp
@@ -10,14 +10,9 @@
 
 #include <iostream>
 
-#include "menubar.h"
-#include "viewport.h"
-#include "config_control.h"
-#include "cube.h"
-#include "mesh.h"
-#include "view.h"
 #include "shaderclass.h"
 #include "camera.h"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
@@ -35,6 +30,10 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+// ImGui-controlled state
+bool wireframeMode = false;
+float bgColor[4] = { 0.10f, 0.10f, 0.12f, 1.0f };
+
 int main()
 {
     if (!glfwInit()) return -1;
@@ -43,7 +42,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Rose Simulation", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Textured Pyramid - 20011.jpg", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cerr << "Failed to create GLFW window\n";
@@ -55,81 +54,161 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD\n";
+        return -1;
+    }
 
     glEnable(GL_DEPTH_TEST);
 
-    {
-        Shader ourShader("shaders/shader.vert", "shaders/shader.frag");
+    Shader ourShader("shaders/shader.vert", "shaders/shader.frag");
 
-        Cube cube;
-        cube.Init("20011.jpg");
+    // Pyramid: 4 side faces + square base (2 triangles) = 6 triangles = 18 vertices
+    // Each vertex: position (3) + texcoord (2)
+    float pyramidVertices[] = {
+        // Front face
+         0.0f,  0.5f,  0.0f,   0.5f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        // Right face
+         0.0f,  0.5f,  0.0f,   0.5f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        // Back face
+         0.0f,  0.5f,  0.0f,   0.5f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        // Left face
+         0.0f,  0.5f,  0.0f,   0.5f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        // Base - triangle 1
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        // Base - triangle 2
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidVertices), pyramidVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+
+    // load texture 20011.jpg
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* data = stbi_load("20011.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        std::cout << "Loaded texture 20011.jpg: " << width << "x" << height << " channels=" << nrChannels << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed to load texture 20011.jpg\n";
+    }
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setVec3("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
+    ourShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+
+    // Setup ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        glfwPollEvents();
+        processInput(window);
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Settings");
+        ImGui::Text("Pyramid - 20011.jpg");
+        ImGui::Separator();
+        ImGui::Checkbox("Wireframe Mode", &wireframeMode);
+        ImGui::ColorEdit4("Background Color", bgColor);
+        ImGui::Separator();
+        ImGui::Text("FPS: %.1f", io.Framerate);
+        ImGui::End();
+
+        int currentWidth, currentHeight;
+        glfwGetFramebufferSize(window, &currentWidth, &currentHeight);
+        glViewport(0, 0, currentWidth, currentHeight);
+
+        glClearColor(bgColor[0], bgColor[1], bgColor[2], bgColor[3]);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        if (wireframeMode)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        else
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+        float aspectRatio = (currentHeight > 0) ? ((float)currentWidth / (float)currentHeight) : 1.0f;
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), aspectRatio, 0.1f, 100.0f);
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 model = glm::mat4(1.0f); // centered at origin, no spin
 
         ourShader.use();
-        ourShader.setInt("texture1", 0);
-        ourShader.setVec3("objectColor", glm::vec3(1.0f, 1.0f, 1.0f));
-        ourShader.setVec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("model", model);
+        ourShader.setInt("useTexture", 1);
 
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-        ImGuiIO& io = ImGui::GetIO(); (void)io;
-        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-        ImGui::StyleColorsDark();
-        ImGui_ImplGlfw_InitForOpenGL(window, true);
-        ImGui_ImplOpenGL3_Init("#version 330");
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 18);
 
-        while (!glfwWindowShouldClose(window))
-        {
-            float currentFrame = static_cast<float>(glfwGetTime());
-            deltaTime = currentFrame - lastFrame;
-            lastFrame = currentFrame;
+        // Ensure ImGui is rendered filled
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-            glfwPollEvents();
-            processInput(window);
-
-            ImGui_ImplOpenGL3_NewFrame();
-            ImGui_ImplGlfw_NewFrame();
-            ImGui::NewFrame();
-
-            MenuBar::Draw(window);
-            Viewport::DrawWindow();
-            ViewMenu::DrawWindow();
-
-            int currentWidth, currentHeight;
-            glfwGetFramebufferSize(window, &currentWidth, &currentHeight);
-            glViewport(0, 0, currentWidth, currentHeight);
-
-            glClearColor(ConfigControl::clearColor[0], ConfigControl::clearColor[1], ConfigControl::clearColor[2], ConfigControl::clearColor[3]);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-            if (ConfigControl::wireframemode)
-                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            else
-                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-            float aspectRatio = (currentHeight > 0) ? ((float)currentWidth / (float)currentHeight) : 1.0f;
-            glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), aspectRatio, 0.1f, 100.0f);
-            glm::mat4 view = camera.GetViewMatrix();
-
-            float rotation[3] = { ConfigControl::GetCurrentRotX(), ConfigControl::GetCurrentRotY(), ConfigControl::GetCurrentRotZ() };
-            float cubeColor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
-
-            ourShader.use();
-            ourShader.setMat4("projection", projection);
-            ourShader.setMat4("view", view);
-
-            cube.Draw(ourShader, cubeColor, rotation, ConfigControl::showMultipleCubes);
-
-            MeshMenu::DrawGL(ourShader);
-
-            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-            ImGui::Render();
-            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-            glfwSwapBuffers(window);
-        }
+        glfwSwapBuffers(window);
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteTextures(1, &texture);
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
